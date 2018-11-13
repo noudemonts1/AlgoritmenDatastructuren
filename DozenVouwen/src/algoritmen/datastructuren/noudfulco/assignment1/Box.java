251ca23bbcaddbc2eb4802d1532ab457329d9f72
noudemonts1 <36259968+noudemonts1@users.noreply.github.com>
2018-11-13 22:54:42 +0100

    Got it to work, now optimize

--- DozenVouwen/src/algoritmen/datastructuren/noudfulco/assignment1/Box.java
+++ DozenVouwen/src/algoritmen/datastructuren/noudfulco/assignment1/Box.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.lang.*;
 
-public class Box implements Comparable<Box>{
+public class Box implements Comparable<Box> {
 
 	private float[] dimensions;
 	private boolean isSource;
@@ -12,13 +12,22 @@ public class Box implements Comparable<Box>{
 	private Box predecessor;
 	private boolean isNested;
 
-	public Box(){
-		dimensions = new float[]{0,0,0};
+	/**
+	 * When no parameters are given, we want to create the Source vertice. This
+	 * vertice is initialized with no dimensions (or [0, 0, 0]), distance equal to
+	 * 0, isSource set to true, and isNested set to false.
+	 */
+	public Box() {
+		dimensions = new float[] { 0, 0, 0 };
 		distance = 0;
 		isSource = true;
-		isNested = false;
+		isNested = true;
 	}
 
+	/**
+	 * When the dimensions parameter is given, we create a vertice that with given
+	 * @param dimensions
+	 */
 	public Box(float[] dimensions) {
 		this.dimensions = dimensions;
 		Arrays.sort(dimensions);
@@ -31,30 +40,35 @@ public class Box implements Comparable<Box>{
 		return dimensions;
 	}
 
+	/**
+	 * Checks if this box fits in another box
+	 * @param other box
+	 * @return
+	 */
 	public boolean checkFit(Box other) {
 		float[] otherDims = other.getDimensions();
-		for(int i = 0; i < dimensions.length; i++) {
-			if(dimensions[i] >= otherDims[i])
+		for (int i = 0; i < dimensions.length; i++) {
+			if (dimensions[i] >= otherDims[i])
 				return false;
 		}
 		return true;
 	}
-	
+
 	public float getDistance() {
 		return distance;
 	}
-	
+
 	public void setDistance(float distance) {
 		this.distance = distance;
 	}
-	
+
 	public void setPredecessor(Box box) {
 		this.predecessor = box;
 	}
 
 	@Override
 	public String toString() {
-		return dimensions[0] + " " + dimensions[1] + " " + dimensions[2] ;
+		return dimensions[0] + " " + dimensions[1] + " " + dimensions[2];
 	}
 
 	@Override
@@ -74,8 +88,6 @@ public class Box implements Comparable<Box>{
 
 	public void setNested() {
 		isNested = true;
-		if (isSource)
-			System.out.println("Source set to nested");
 	}
 
 	public boolean getNested() {
--- DozenVouwen/src/algoritmen/datastructuren/noudfulco/assignment1/Graph.java
+++ DozenVouwen/src/algoritmen/datastructuren/noudfulco/assignment1/Graph.java
@@ -15,12 +15,10 @@ public class Graph {
 	}
 
 	public void createGraph() {
-		int listSize = boxes.length + 1 - nestedBoxes;
-//		System.out.println("Listsize is: " + listSize);
-//		System.out.println("Boxes: " + boxes.length);
-//		System.out.println("NestedBoxes: " + nestedBoxes);
+		int listSize = boxes.length - nestedBoxes + 1;
 		adjListArray = new LinkedList[listSize];
 		adjListArray[0] = new LinkedList<>();
+		
 		// Add source vertex
 		Box source = new Box();
 		adjListArray[0].add(source);
@@ -30,17 +28,24 @@ public class Graph {
 			}
 		}
 		// Add all other vertices
-		int step = 0;
+		int index = 1;
 		for (int i = 0; i < boxes.length; i++) {
 			//checken of dit beter kan dan met steps
 			if (!boxes[i].getNested()) {
-				adjListArray[step + 1] = new LinkedList<>();
-				adjListArray[step + 1].add(boxes[i]);
+				//System.out.println("Step: " + index);
+				//System.out.println("Box that is not nested: [" + boxes[i] + "]");
+				boxes[i].setPredecessor(source);
+				boxes[i].setDistance(Float.POSITIVE_INFINITY);
+				adjListArray[index] = new LinkedList<>();
+				adjListArray[index].add(boxes[i]);
 				for (int j = i + 1; j < boxes.length; j++) {
-					if (boxes[i].checkFit(boxes[j]) && !boxes[i].getNested())
-						adjListArray[step + 1].add(boxes[j]);
+					if (boxes[i].checkFit(boxes[j]) && !boxes[j].getNested()) {
+						//System.out.println("[" + boxes[j] + "] is not yet nested and added");
+						adjListArray[index].add(boxes[j]);
+					}
 				}
-				step++;
+				//printGraph();
+				index++;
 			}
 		}
 	}
@@ -59,6 +64,7 @@ public class Graph {
 		while (!box.getIsSource()) {
 			Box pre = box.getPredecessor();
 			box.setNested();
+			//System.out.println("Nested box: " + box);
 			box = pre;
 			nestedBoxes++;
 		}
@@ -67,28 +73,21 @@ public class Graph {
 	public Box findShortestDist() {
 		float distance = Float.POSITIVE_INFINITY;
 		Box box = adjListArray[0].get(0);
-//		int count = 0;
 		for (int i = 1; i < adjListArray.length; i++) {
 			if (adjListArray[i].get(0).getDistance() < distance) {
-//				count++;
 				distance = adjListArray[i].get(0).getDistance();
 				box = adjListArray[i].get(0);
-//				System.out.println(distance);
 			}
 		}
-//		System.out.println(count);
 		return box;
 	}
 
 	public void relax(Box boxA, Box boxB) {
 		float weight = -1;
-//		System.out.println("Hoe lang is een chinees");
 		if (boxA.getIsSource() || boxB.getIsSource()) {
-//			System.out.println("Relax if statement 1");
 			weight = 0;
 		}
 		if (boxB.getDistance() > boxA.getDistance() + weight) {
-//			System.out.println("Relax if statement 2");
 			boxB.setDistance(boxA.getDistance() + weight);
 			boxB.setPredecessor(boxA);
 		}
@@ -96,7 +95,6 @@ public class Graph {
 
 	public void dagShortestPaths() {
 		for (int i = 0; i < adjListArray.length; i++) {
-			//System.out.println("List size: " + adjListArray[i].size());
 			if (adjListArray[i].size() > 1) {
 				for (int j = 1; j < adjListArray[i].size(); j++) {
 					relax(adjListArray[i].get(0), adjListArray[i].get(j));
